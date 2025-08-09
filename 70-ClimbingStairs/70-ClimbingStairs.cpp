1fb2a44511eea7d23d7e100d95befe002106ebf6
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-08-09 17:19:26 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/9/2025, 5:19:25 PM
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1){
+            return 1;
+        }
+        int a=1;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+};
