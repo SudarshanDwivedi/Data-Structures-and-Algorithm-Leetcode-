bc7bc44bf2dd3b07eeb7b671852af883e28229a1
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-08-09 17:19:36 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/9/2025, 5:19:35 PM
+class Solution {
+public:
+    double myPow(double x, int n) {
+         long long N = n;  
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1.0;
+        while (N > 0) {
+            if (N % 2 == 1) {  
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+
+        return result;
+    }
+};
