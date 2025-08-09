b99a9aea3d879d8aaf311df9011ec818a9af762e
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-08-09 17:19:16 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -0,0 +1,38 @@
+// Last updated: 8/9/2025, 5:19:15 PM
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+
+        int left = 0, right = s.length()-1;
+        while(left<right)
+        {
+            if(!isalnum(s[left])) 
+                left++;
+            else if(!isalnum(s[right])) 
+                right--;
+            else if(tolower(s[left])!=tolower(s[right])) 
+                return false;
+            else {
+                left++; 
+                right--;
+            }
+        }
+        return true;
+
+}
+int main() {
+
+  string str = "ABCDCBA";
+  bool ans = isPalindrome(str);
+
+  if (ans == true) {
+    cout << "Palindrome";
+  } else {
+    cout << "Not Palindrome";
+  }
+  return 0;
+}
+        
+    
+};
