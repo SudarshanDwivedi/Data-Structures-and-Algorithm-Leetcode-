69e709ab2f93eeb9f2cb2fd6a79d77051c032c91
Ziegler_Natta <153724823+SudarshanDwivedi@users.noreply.github.com>
2025-08-09 17:19:28 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/9/2025, 5:19:27 PM
+class Solution {
+public:
+    int mySqrt(int x) {
+                if (x < 2) return x;
+
+        int left = 1, right = x / 2, result = 0;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long square = mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                result = mid; 
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+
+    }
+};
